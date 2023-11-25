e557d484a05932e2a999366c09dc0e3add558c5b
Adam Momen <adammuman81@gmail.com>
2023-11-25 14:47:08 +0800

    Add: deployed website source origin url

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const { Server, Origins } = require("boardgame.io/server");
 const { TicTacToe } = require("./Game");
 
-const server = Server({ games: [TicTacToe], origins: [Origins.LOCALHOST] });
+const server = Server({
+  games: [TicTacToe],
+  origins: [
+    Origins.LOCALHOST,
+    Origins.LOCALHOST_IN_DEVELOPMENT,
+    "https://grid-master.vercel.app",
+  ],
+});
 
 server.run(8000);
